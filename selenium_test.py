28c60a2cf689b0abf01b55bb88feadd4c2026f5e
Mist-Rain <mr.illbeback@gmail.com>
2020-01-08 02:31:48 +0800

    no message

--- selenium_test.py
+++ selenium_test.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 import time
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+import requests
 import os
 os.makedirs('./download/', exist_ok=True)
 
