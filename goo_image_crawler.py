9c2abbc6dddf5c599806ed0a2d3d8c7c9e6f944c
Mist-Rain <mr.illbeback@gmail.com>
2020-01-07 17:20:13 +0800

    no message

--- goo_image_crawler.py
+++ goo_image_crawler.py
@@ -3,15 +3,28 @@ import sys
 from bs4 import BeautifulSoup
 import random
 
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36'}
+headers = {}
+
+#Connection = 'keep-alive'
+#Accept = 'text/html, application/xhtml+xml, application/xml;q=0.9, image/webp, */*;q=0.8'
+ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36'
+#Accept_Encoding = 'gzip, deflate, sdch'
+#Accept_Language = 'en-US,en;q=0.8'
+
+#headers['Connection']=Connection
+#headers['Accept']=Accept
+headers['User-Agent']=ua
+#headers['Accept-Encoding']=Accept_Encoding
+#headers['Accept-Language']=Accept_Language
+
 data_src_list = []
 
 def gooImage(search_text):
     search_text = search_text.replace('\"','')
-    url = 'https://www.google.com.tw/search?tbm=isch&q='+search_text
+    url = 'https://www.google.co.jp/search?tbm=isch&q='+search_text
     # 下載網站內容
     res = requests.get(url, headers=headers)
-    res.encoding = 'utf-8'
+    #res.encoding = 'utf-8'
     
     # 確認是否下載成功
     if res.status_code == requests.codes.ok:
@@ -29,8 +42,9 @@ def gooImage(search_text):
             #print(len(data_src_list))
         except:
             gooImage(sys.argv[1])
-            #gooImage('詩乃')
+            #gooImage('本本')
 
-sys.stdout.reconfigure(encoding='utf-8')
+#sys.stdout.reconfigure(encoding='utf-8')
 gooImage(sys.argv[1])
-#gooImage('詩乃')
+#gooImage('本本')
+#print(headers)
