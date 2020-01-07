168681557a0c929f3ab2e1d2555a4191cc2e8b28
Mist-Rain <mr.illbeback@gmail.com>
2020-01-08 02:01:04 +0800

    no message

--- beauty_crawler.py
+++ beauty_crawler.py
@@ -1,3 +1,5 @@
+# 爬ptt表特版圖片並下載
+
 import requests
 import sys
 from bs4 import BeautifulSoup
@@ -7,16 +9,17 @@ import os
 os.makedirs('./image/', exist_ok=True)
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36'}
-PPT_URL = 'https://www.ptt.cc'
-beauty_url_list = []
-img_list = []
-page_list = []
+PTT_URL = 'https://www.ptt.cc'
 
-def getRandomPage(page_num):
-  url = PPT_URL + '/bbs/Beauty/index.html'
+# 回傳表特版前n頁網址list
+def getPageList(page_num):
+  url = PTT_URL + '/bbs/Beauty/index.html'
+  
   # 下載網站內容
   res = requests.get(url, headers=headers, cookies={'over18': '1'})
   res.encoding = 'utf-8'
+  
+  page_list = []
   # 確認是否下載成功
   if res.status_code == requests.codes.ok:
     # 以 BeautifulSoup 解析 HTML 程式碼
@@ -29,7 +32,7 @@ def getRandomPage(page_num):
           index_num = int(index_str)+1
           #print(index_num)
           for i in range(page_num):
-            page_list.append(PPT_URL+ '/bbs/Beauty/index'+str(index_num-i)+'.html')
+            page_list.append(PTT_URL+ '/bbs/Beauty/index'+str(index_num-i)+'.html')
           #print(page_list)
           return page_list          
     except:
@@ -37,53 +40,81 @@ def getRandomPage(page_num):
   else:
     print('Invalid url: '+url)
 
+# 回傳文章標題url
 def getTitleUrlList(page_list):
-  url = random.choice(page_list)
-  # 下載網站內容
-  res = requests.get(url, headers=headers, cookies={'over18': '1'})
-  res.encoding = 'utf-8'
+	title_url_list = []
+	# 下載網站內容
+	for url in page_list:
+		res = requests.get(url, headers=headers, cookies={'over18': '1'})
+		res.encoding = 'utf-8'
 
-  # 確認是否下載成功
-  if res.status_code == requests.codes.ok:
-    # 以 BeautifulSoup 解析 HTML 程式碼
-    try:
-      soup = BeautifulSoup(res.text, 'html.parser')
-      for item in soup.find_all('div', class_='title'):
-        if item.text[2:4] == '正妹' or item.text[2:4] == '神人':
-          #print(item.find('a').text)
-          #print(PPT_URL+item.find('a').get('href'))
-          beauty_url_list.append(PPT_URL+item.find('a').get('href'))
-      #print(beauty_url_list)
-      return beauty_url_list
-    except:
-      print('get title error')
-  else:
-    print('Invalid url: '+url)
+		# 確認是否下載成功
+		if res.status_code == requests.codes.ok:
+			# 以 BeautifulSoup 解析 HTML 程式碼
+			try:
+				soup = BeautifulSoup(res.text, 'html.parser')
+				for item in soup.find_all('div', class_='title'):
+					if item.text[2:4] == '正妹' or item.text[2:4] == '神人':
+						#print(item.find('a').text)
+						#print(PPT_URL+item.find('a').get('href'))
+						title_url_list.append(PTT_URL+item.find('a').get('href'))
+						#print(item.find('a').get('href'))
+				#print(beauty_url_list)
+			except:
+				print('get title error')
+		else:
+			print('Invalid url: '+url)
+	return title_url_list
 
-def getRandomImage(url_list):
-  random_url = random.choice(url_list)
-  res = requests.get(random_url, headers=headers, cookies={'over18': '1'})
-  res.encoding = 'utf-8'
-  # 確認是否下載成功
-  if res.status_code == requests.codes.ok:
-    # 以 BeautifulSoup 解析 HTML 程式碼
-    try:
-      soup = BeautifulSoup(res.text, 'html.parser')
-      for item in soup.find_all('div', id='main-content'):
-        for i in item.find_all('a'):
-          if(i.parent.name != 'span'):
-            if(i.get('href')[len(i.get('href'))-4:] == '.jpg'):
-              img_list.append(i.get('href'))
-      #print(img_list)
-      random_img = random.choice(img_list)
-      print(random_img)
-      '''img_res = requests.get(random_img)
-      with open('./image/img.png', 'wb') as f:
-        f.write(img_res.content)'''
-    except:
-      getRandomImage(getTitleUrlList(getRandomPage(200)))
-  else:
-    print('Invalid url: '+url)
+# 回傳文章內圖片url
+def getImageUrlList(title_url_list):
+	img_url_list = []
+	for url in title_url_list:
+		res = requests.get(url, headers=headers, cookies={'over18': '1'})
+		res.encoding = 'utf-8'
 
-#sys.stdout.reconfigure(encoding='utf-8')
-getRandomImage(getTitleUrlList(getRandomPage(200)))
+		# 確認是否下載成功
+		if res.status_code == requests.codes.ok:
+			# 以 BeautifulSoup 解析 HTML 程式碼
+			try:
+				soup = BeautifulSoup(res.text, 'html.parser')
+				for item in soup.find_all('div', id='main-content'):
+					for i in item.find_all('a'):
+						if(i.parent.name != 'span'):
+							if(i.get('href')[len(i.get('href'))-4:] == '.jpg'):
+								img_url_list.append(i.get('href'))
+								#print(i.get('href'))
+			except:
+				print('get image error')
+		else:
+			print('Invalid url: '+url)
+	return img_url_list
+
+def randomImage(num):
+	#sys.stdout.reconfigure(encoding='utf-8')
+	page_list = getPageList(num)
+	page = [random.choice(page_list)]
+	
+	title_list = getTitleUrlList(page)
+	title = [random.choice(title_list)]
+	
+	try:
+		img_url_list = getImageUrlList(title)
+		print(random.choice(img_url_list))
+	except:
+		randomImage(num)
+	return img_url_list
+	
+def download(url_list):
+	count = 1
+	for url in url_list:
+		img_res = requests.get(url)
+		with open('./image/'+str(count)+'.png', 'wb') as f:
+			f.write(img_res.content)
+		print("%s download"%url)
+		count+=1
+	return
+	
+if __name__ == '__main__':
+	randomImage(1)
+	
